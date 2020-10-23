700ec8b26a7b91e9cc549e23e1b3bbeedc14614a
ryanpurchase288 <ryanpurchase288.rp@gmail.com>
2020-10-23 15:25:17 +0100

    adding the intro tutorial

--- near.py
+++ near.py
@@ -2,6 +2,8 @@
   
 def near(string1, string2):
     for i in range(len(string1)):
+        print(string1[:i])
+        print(string1[i+1:])
         newstr = string1[:i] + string1[i+1:]
         print(newstr)
         if newstr == string2:
---
+++ python-intro.py
@@ -0,0 +1,11 @@
+# print('Hello World')
+
+#firstname ='Ryan'
+#surname ='Purchase'
+#print('Hello '+firstname+" "+surname)
+
+number1 = float(input("Please eneter the first number: "))
+number2 = float(input("Please enter the second number: "))
+answer = number1 + number2
+
+print(number1, "+", number2, "=", answer)
