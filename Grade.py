469d646a749992cd22da6a232f5dfd60ad3ca794
ryanpurchase288 <ryanpurchase288.rp@gmail.com>
2020-10-26 12:30:15 +0000

    Complete grade exercise for functions on community

---
+++ Grade.py
@@ -0,0 +1,33 @@
+def calculator(homework,assessment,exam):
+     total= (homework+assessment+exam)/175
+     total=total*100
+     return total
+
+def grade(score):   
+ if score>=70 :
+    return"A"
+ elif score>=60 :
+    return"B"
+ elif score>=50 :
+    return"C"
+ elif score>=40 :
+    return"D"
+ else :
+    return"F"
+
+
+
+numberOfStudents = int(input('Enter how many students:'))
+
+for count in range (numberOfStudents):
+ name = input('enter students name:')
+ homeworkScore = int(input('Enter homework score /25:'))
+ assesmentScore = int(input('Enter assessment score /50:'))
+ examScore = int(input('Enter exam score /100:'))
+
+ percentage=int(calculator(homeworkScore,assesmentScore,examScore))
+ gradeAchieved = str(grade(percentage))
+ print(name, 'percentage is:', percentage,'% and achieved a grade of:', gradeAchieved)
+
+
+    
--- near.py
+++ near.py
@@ -2,8 +2,6 @@
   
 def near(string1, string2):
     for i in range(len(string1)):
-        print(string1[:i])
-        print(string1[i+1:])
         newstr = string1[:i] + string1[i+1:]
         print(newstr)
         if newstr == string2:
