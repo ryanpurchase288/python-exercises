25e8e8101cbbb4c0a4de0407b1c9144b28e9a4bb
ryanpurchase288 <ryanpurchase288.rp@gmail.com>
2020-10-26 16:42:14 +0000

    fixed error

--- number2wordsModule.py
+++ number2wordsModule.py
@@ -6,7 +6,7 @@ def single(numberList):
 
 
 def teens(numberList):
-    teenage = {"10": "Ten", "11": "Eleven", "12": "Twelve", "13": "Thirteen", "14": "Fourteen", "15": "Fifteen",
+    teen = {"10": "Ten", "11": "Eleven", "12": "Twelve", "13": "Thirteen", "14": "Fourteen", "15": "Fifteen",
                "16": "Sixteen", "17": "Seventeen", "18": "Eighteen", "19": "Nineteen"}
     if numberList in list(teenage.keys()):
         return teen[numberList]
